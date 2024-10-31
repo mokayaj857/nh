ca1cf4a8285692f9dc04da1c2f70fd624b5bac12
amschel99 <6290.2020@students.ku.ac.ke>
2024-10-31 21:40:56 +0300

    zkgm! added some wallet functions like spending utxos

--- src/forge_backend/src/lib.rs
+++ src/forge_backend/src/lib.rs
@@ -1,14 +1,17 @@
 use ::bitcoin::Network;
 use candid::{CandidType, Deserialize};
 use ic_cdk::api::management_canister::bitcoin::{
-    BitcoinNetwork, GetUtxosResponse, MillisatoshiPerByte,
+    bitcoin_get_balance, bitcoin_get_utxos, BitcoinNetwork, GetBalanceRequest, GetUtxosRequest,
+    GetUtxosResponse, MillisatoshiPerByte,
 };
+
 use ic_cdk::{init, update};
 
 use std::cell::{Cell, RefCell};
 
 mod bitcoin;
 mod ecdsa_api;
+mod wallet;
 
 thread_local! {
 
@@ -41,4 +44,37 @@ pub async fn get_p2pkh_address() -> String {
     let network = NETWORK.with(|n| n.get());
     bitcoin::get_address(network, key_name, derivation_path).await
 }
+#[update]
+pub async fn get_balance(address: String) -> u64 {
+    let network = NETWORK.with(|n| n.get());
+    _get_balance(network, address).await
+}
+pub async fn _get_balance(network: BitcoinNetwork, address: String) -> u64 {
+    let min_confirmations = None;
+    let balance_res = bitcoin_get_balance(GetBalanceRequest {
+        address,
+        network,
+        min_confirmations,
+    })
+    .await;
+
+    balance_res.unwrap().0
+}
+#[update]
+pub async fn get_utxos(address: String) -> GetUtxosResponse {
+    let network = NETWORK.with(|n| n.get());
+    _get_utxos(network, address).await
+}
+pub async fn _get_utxos(network: BitcoinNetwork, address: String) -> GetUtxosResponse {
+    let filter = None;
+    let utxos_res = bitcoin_get_utxos(GetUtxosRequest {
+        address,
+        network,
+        filter,
+    })
+    .await;
+
+    utxos_res.unwrap().0
+}
+
 ic_cdk::export_candid!();
