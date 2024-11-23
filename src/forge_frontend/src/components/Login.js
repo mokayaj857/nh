9e04ae983496a4372e20e6feab26f38f81952bc8
Bratipah <bratipahmh@gmail.com>
2024-11-23 23:58:50 +0300

    update staking app

---
+++ src/forge_frontend/src/components/Login.js
@@ -0,0 +1,61 @@
+import { Actor } from "@dfinity/agent";
+import { AuthClient } from "@dfinity/auth-client";
+import { NFID_backend } from "../../declarations/NFID_backend";
+
+let authClient = null;
+
+async function init() {
+  authClient = await AuthClient.create();
+}
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await NFID_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
+
+function handleSuccess() {
+  const principalId = authClient.getIdentity().getPrincipal().toText();
+
+  document.getElementById(
+    "principalId"
+  ).innerText = `Your PrincipalId: ${principalId}`;
+
+  Actor.agentOf(NFID_backend).replaceIdentity(
+    authClient.getIdentity()
+  );
+}
+
+document.getElementById("login").addEventListener("click", async (e) => {
+  if (!authClient) throw new Error("AuthClient not initialized");
+
+  const APP_NAME = "NFID example";
+  const APP_LOGO = "https://nfid.one/icons/favicon-96x96.png";
+  const CONFIG_QUERY = `?applicationName=${APP_NAME}&applicationLogo=${APP_LOGO}`;
+
+  const identityProvider = `https://nfid.one/authenticate${CONFIG_QUERY}`;
+
+  authClient.login({
+    identityProvider,
+    onSuccess: handleSuccess,
+    windowOpenerFeatures: `
+      left=${window.screen.width / 2 - 525 / 2},
+      top=${window.screen.height / 2 - 705 / 2},
+      toolbar=0,location=0,menubar=0,width=525,height=705
+    `,
+  });
+});
+
+init();
